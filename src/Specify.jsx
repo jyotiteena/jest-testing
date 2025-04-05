a58061c6ea862ae0d7f1045e59212b8cef92861a
jyotiteena <jingarjyoti13@gmail.com>
2025-04-05 12:34:14 +0530

    test on specify files

---
+++ src/Specify.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Specify = () => {
+    return (
+        <div>
+            How to run specific test files.
+            What is watch Mode?
+            How to run the failed test case.
+            How to run all test cases.
+            How to quit watch mode.
+            How to filter test files for run.
+            How to Filter test case.
+
+
+            Watch Usage
+            › Press a to run all tests.
+            › Press f to run only failed tests.
+            › Press q to quit watch mode.
+            › Press p to filter by a filename regex pattern.
+            › Press t to filter by a test name regex pattern.
+            › Press Enter to trigger a test run.
+
+        </div>
+    )
+}
+
+export default Specify
